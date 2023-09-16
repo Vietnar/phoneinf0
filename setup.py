a294952ad6157f7febf18719402e4f3b69cb083d
Vietnar <145231143+Vietnar@users.noreply.github.com>
2023-09-17 00:29:41 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("clear && pip install phonenumbers")
+os.system("clear && pip install colorama")
+os.system("clear && pip install carrier")
+os.system("clear && pip install timezone")
+os.system("clear && pip install geocoder")
